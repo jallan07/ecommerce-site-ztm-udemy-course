9a1532494f759ea80feb697de8e9a9535444fc32
Joshua Allan <joshuaallan@joshuas-air-2.lan>
2021-03-19 15:37:02 -0400

    Finialized the connection of firebase and the sign in with google option. Users who select that auth method will be added to the db automatically.

--- client/src/App.js
+++ client/src/App.js
@@ -10,7 +10,7 @@ import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import Header from './components/header/header.component';
 
-import { auth } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 class App extends Component {
   constructor() {
@@ -24,9 +24,20 @@ class App extends Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged((user) => {
-      this.setState({ currentUser: user });
-      console.log(user);
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+        userRef.onSnapshot((snapShot) => {
+          this.setState({
+            // set the current user to the snapshot that we receive and the snapshot's data that is received
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data(),
+            },
+          });
+        });
+      }
+      this.setState({ currentUser: userAuth });
     });
   }
 
--- client/src/firebase/firebase.utils.js
+++ client/src/firebase/firebase.utils.js
@@ -11,8 +11,43 @@ const config = {
   appId: '1:505468296549:web:7042bc676d1481db15558c',
 };
 
+// the async function below takes in a user object when they sign in with Google, and creates a profile for that user in our users collection in Firestore
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  // check to make sure we are getting back a valid object; if it doesn't exist return out of the function
+  if (!userAuth) return;
+  // query inside of firestore for the document to see if it already exists
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+  const snapShot = await userRef.get();
+
+  // if there isn't an existing user, then create a new one
+  if (!snapShot.exists) {
+    // get these details off the userAuth object
+    const { displayName, email } = userAuth;
+    // tells us the exact time and date in which the account was created
+    const createdAt = new Date();
+
+    try {
+      // create the new user in the database
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData,
+      });
+    } catch (err) {
+      console.log('error creating user', err.message);
+    }
+  }
+
+  // we might still want the userRef object for use throughout the app, so we return it out of the function
+  return userRef;
+};
+
 firebase.initializeApp(config);
 
+// set up the auth and firestore variables for use throughout the app
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
